1d990dd1f7fe45311c45f35fdafc15e69cf6ef9e
Lucas Lima Mota <lucas.mota@fitbank.com.br>
2021-10-15 17:08:46 -0300

    Metade feito

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,72 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Jogo da forca</h1>
+
+    <section v-if="tela === 'inicio'" id="inicio">
+      <Formulario v-if="etapa === 'palavra'"
+        title="Defina a palavra"
+        button="Próximo" 
+        :action ="setPalavra"
+        />
+
+        <Formulario v-if="etapa === 'dica'"
+        title="Defina a dica"
+        button="Iniciar Jogo" 
+        :action ="setDica"/>
+
+
+    </section>
+
+    <section v-if="tela === 'jogo'" id="jogo">
+      <Jogo 
+      :erros = "erros"
+      />
+    </section>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import "./css/global.css";
+
+import Formulario from './components/Formulario.vue';
+import Jogo from './components/Jogo.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      tela: "inicio",
+      etapa: 'palavra',
+      palavra:'',
+      dica:'',
+      erros: 3
+    };
+  },
+  components: {Formulario, Jogo},
+
+  methods:{
+    setPalavra(palavra){
+      this.palavra = palavra;
+      this.etapa = 'dica'
+    },
+
+    setDica(dica){
+      this.dica = dica;
+      this.tela = 'jogo';
+      this.etapa = 'jogo'
+    }
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 }
 </style>
---
+++ src/components/Jogo.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="jogo">
+    <Forca
+    :erros = "erros"
+    />
+  </div>
+</template>
+
+<script>
+import Forca from './Forca.vue'
+export default {
+  name: "Jogo",
+  props:{
+      erros: Number
+  },
+  data() {
+      return {
+          
+      }
+  },
+  components:{Forca},
+  methods:{
+      
+  }
+};
+</script>
+
+<style>
+.jogo{
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+}
+
+</style>
